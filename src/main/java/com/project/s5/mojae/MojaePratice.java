eca135b58e4a1976e36fe03825d1b1d50719b063
mjtas20 <71800729+mjtas20@users.noreply.github.com>
2020-11-04 15:38:09 +0900

    morempratice2

---
+++ src/main/java/com/project/s5/mojae/MojaePratice.java
@@ -0,0 +1,17 @@
+package com.project.s5.mojae;
+
+import java.lang.annotation.Target;
+
+import javax.annotation.processing.Generated;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value ="morem")
+public class MojaePratice {
+  
+	public void Test() throws Exception{
+		
+	}
+}
